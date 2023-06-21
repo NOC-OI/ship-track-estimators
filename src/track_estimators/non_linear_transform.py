59f728abc804d8e82f66a20c63987738055caf36
João Morado <joaomorado@gmail.com>
2023-06-21 15:42:23 +0100

    Add nonlinear transforms

---
+++ src/track_estimators/non_linear_transform.py
@@ -0,0 +1,62 @@
+import numpy as np
+from constants import EARTH_RADIUS
+
+
+def fx(x, dt, sog_rate=0.0, cog_rate=0):
+    """
+    TODO write docstring.
+
+    Notes
+    -----
+    Dynamics based on the method described in:
+
+    Cole, B.; Schamberg, G.
+    Unscented Kalman Filter for Long-Distance Vessel Tracking in Geodetic Coordinates.
+    Applied Ocean Research 2022, 124, 103205.
+    https://doi.org/10.1016/j.apor.2022.103205.
+
+    Parameters
+    ----------
+    x
+        _description_
+    dt
+        _description_
+    sog_rate, optional
+        _description_, by default 0.
+    cog_rate, optional
+        _description_, by default 0
+
+    Returns
+    -------
+        _description_
+    """
+    lon = x[0]
+    lat = x[1]
+    u = x[2]
+    alpha = x[3]
+
+    # Precompute sin and cos of udt_R
+    udt_R = u * dt / EARTH_RADIUS
+    udt_R_sin = np.sin(udt_R)
+    udt_R_cos = np.cos(udt_R)
+
+    # Eq. (35), lon
+    arctan_term_a = udt_R_sin * np.sin(alpha)
+    arctan_term_b = np.cos(lat) * udt_R_cos - np.sin(lat) * udt_R_sin * np.cos(alpha)
+    transformed_lon = lon + np.arctan2(arctan_term_a, arctan_term_b)
+
+    # Eq. (35), lat
+    transformed_lat = np.sin(lat) * udt_R_cos + np.cos(lat) * udt_R_sin * np.cos(alpha)
+    transformed_lat = np.arcsin(transformed_lat)
+
+    # Eq. (35), u
+    transformed_u = u + sog_rate * dt
+
+    # Eq. (35), alpha
+    transformed_alpha = alpha + cog_rate * dt
+
+    transformed_x = np.array(
+        [transformed_lon, transformed_lat, transformed_u, transformed_alpha]
+    )
+
+    return transformed_x
