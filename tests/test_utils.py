85efd6ce4d99a61d1943d42b07d7b0d7db2a9a81
João Morado <joaomorado@gmail.com>
2023-06-22 18:17:35 +0100

    Add heading test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-from track_estimators.utils import haversine_formula
+from track_estimators.utils import haversine_formula, heading
 
 
 def test_haversine_formula():
@@ -25,3 +25,27 @@ def test_haversine_formula():
     lon = np.random.uniform(-90, 90)
 
     assert np.isclose(haversine_formula(lon, lat, lon, lat), 0.0)
+
+
+def test_heading():
+    """
+    Test the heading function.
+
+    References
+    ----------
+    - https://www.igismap.com/formula-to-find-bearing-or-heading-angle-between-two-points-latitude-longitude/
+
+    """
+    # Check heading between Kansas City (P1) and St. Louis (P2)
+    lat1 = 39.099912
+    lon1 = -94.581213
+    lat2 = 38.627089
+    lon2 = -90.200203
+
+    assert np.isclose(heading(lon1, lat1, lon2, lat2), 96.51, rtol=1e-3)
+
+    # Check heading when two points are identical
+    lat = np.random.uniform(-180, 180)
+    lon = np.random.uniform(-90, 90)
+
+    assert np.isclose(heading(lon, lat, lon, lat), 0.0)
