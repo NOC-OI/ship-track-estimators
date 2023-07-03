9bfe8a172817655c3ba42b0c30acc194c271e918
João Morado <joaomorado@gmail.com>
2023-07-03 17:30:19 +0100

    Main alt updates

--- main_alt.py
+++ main_alt.py
@@ -20,12 +20,12 @@ H = np.diag([1, 1, 0, 0])
 
 # Measurement noice covariance
 corr_coff = 0.0
-err_x, err_y = (0.25, 0.25)
+std_x, std_y = (1e-4, 1e-4)
 
 R = np.array(
     [
-        [err_x * err_x, err_x * err_y * corr_coff, 0.0, 0.0],
-        [err_x * err_y * corr_coff, err_y * err_y, 0.0, 0.0],
+        [std_x * std_x, std_x * std_y * corr_coff, 0.0, 0.0],
+        [std_x * std_y * corr_coff, std_y * std_y, 0.0, 0.0],
         [0.0, 0.0, 0, 0.0],
         [0.0, 0.0, 0.0, 0.0],
     ]
@@ -33,14 +33,16 @@ R = np.array(
 
 
 # Process noise covariance
-Q = np.diag((0.0001, 0.0001, 1e-4, 1e-5))
+Q = np.diag((1e-8, 1e-8, 1e-6, 1e-3))
 
 # Initial state
 x0 = ship_track.z[:, 0].reshape(-1, 1)
 
 # Estimate covariance matrix
 # https://www.researchgate.net/post/Kalman_filter_how_do_I_choose_initial_P_0
-P = np.diag([0.25, 0.25, 0.0, 0.0])  # np.diag(x0[:, 0]**2)
+# This does not seem to affect much the output
+P = np.diag([1, 1, 1, 1])
+# P = np.diag(x0[:, 0]**2)
 
 
 # -------------------------------------------------------------- #
@@ -51,7 +53,8 @@ ukf = UnscentedKalmanFilter(
     H=H, Q=Q, R=R, P=P, x0=x0, non_linear_process=geodetic_dynamics
 )
 
-predictions = ukf.run(180, 1.0, ship_track)
+dt = 1
+predictions = ukf.run(int(200 / dt), dt, ship_track)
 predictions = np.asarray(predictions)
 
 
@@ -82,6 +85,11 @@ plt.xlim(-85, -70)
 plt.ylim(25, 45)
 plt.legend()
 plt.show()
+
+
+# plt.plot(ship_track_original.lon[: len(predictions)] - predictions[:, 0, 0])
+# plt.plot(ship_track_original.lat[: len(predictions)] - predictions[:, 1, 0])
+# plt.show()
 
 
 # plt.plot(ship_track_original.lon - predictions[: len(ship_track_original.lon), 0, 0])
