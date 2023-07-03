25f489598c28685b199b40cb657aed211fdf99b1
João Morado <joaomorado@gmail.com>
2023-07-03 16:51:56 +0100

    Add main_alt

--- main_alt.py
+++ main_alt.py
@@ -5,54 +5,64 @@ from track_estimators.kalman_filters.non_linear_process import geodetic_dynamics
 from track_estimators.kalman_filters.unscented import UnscentedKalmanFilter
 from track_estimators.ship_track import ShipTrack
 
+# Read the subsampled data
 csv_file = "data/modern_ships/WCE5063_subset_downsampled.csv"
 ship_track = ShipTrack(csv_file)
 
+# Read the original data
 ship_track_original = ShipTrack("data/modern_ships/WCE5063_subset.csv")
 
-# Create measurament matrix
-z = ship_track.get_measurements()
-
+# -------------------------------------------------------------- #
+#                     Define the KF matrices                     #
 # -------------------------------------------------------------- #
 # Observation matrix
 H = np.diag([1, 1, 0, 0])
 
-# Measurement Uncertainty
+# Measurement noice covariance
 corr_coff = 0.0
-err_x, err_y = (0.5, 0.5)
+err_x, err_y = (0.25, 0.25)
 
 R = np.array(
     [
         [err_x * err_x, err_x * err_y * corr_coff, 0.0, 0.0],
         [err_x * err_y * corr_coff, err_y * err_y, 0.0, 0.0],
-        [0.0, 0.0, 10, 0.0],
-        [0.0, 0.0, 0.0, 0.5],
+        [0.0, 0.0, 0, 0.0],
+        [0.0, 0.0, 0.0, 0.0],
     ]
 )
 
 
 # Process noise covariance
-Q = np.diag(R**2)
+Q = np.diag((0.0001, 0.0001, 1e-4, 1e-5))
+
+# Initial state
+x0 = ship_track.z[:, 0].reshape(-1, 1)
 
 # Estimate covariance matrix
 # https://www.researchgate.net/post/Kalman_filter_how_do_I_choose_initial_P_0
-P = np.diag([err_x, err_y, R[2, 2], R[3, 3]])
-print(P)
-# Initial state
-x0 = z[:, 0].reshape(-1, 1)
-# -------------------------------------------------------------- #
+P = np.diag([0.25, 0.25, 0.0, 0.0])  # np.diag(x0[:, 0]**2)
 
-# Create the unscented kalman filter
+
+# -------------------------------------------------------------- #
+#                     Unscented Kalman Filter                    #
+# -------------------------------------------------------------- #
+# Create and run the unscented kalman filter
 ukf = UnscentedKalmanFilter(
     H=H, Q=Q, R=R, P=P, x0=x0, non_linear_process=geodetic_dynamics
 )
 
-predictions = []
+predictions = ukf.run(180, 1.0, ship_track)
+predictions = np.asarray(predictions)
 
 
+# -------------------------------------------------------------- #
+#                         Plot the results                       #
+# -------------------------------------------------------------- #
 ms = 10
 alpha = 0.4
 
+
+plt.figure(figsize=(10, 10))
 ax = plt.axes(projection=ccrs.PlateCarree())
 ax.stock_img()
 ax.scatter(
@@ -74,6 +84,6 @@ plt.legend()
 plt.show()
 
 
-plt.plot(ship_track_original.lon - predictions[: len(ship_track_original.lon), 0, 0])
-plt.plot(ship_track_original.lat - predictions[: len(ship_track_original.lat), 1, 0])
-plt.show()
+# plt.plot(ship_track_original.lon - predictions[: len(ship_track_original.lon), 0, 0])
+# plt.plot(ship_track_original.lat - predictions[: len(ship_track_original.lat), 1, 0])
+# plt.show()
