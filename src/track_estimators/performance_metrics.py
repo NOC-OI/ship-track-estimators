b063b4ae4b19f3ed5fc4691f4bfe1370be84df74
João Morado <joaomorado@gmail.com>
2023-07-26 11:27:22 +0100

    Add performance metric

---
+++ src/track_estimators/performance_metrics.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+def rmse(x: np.ndarray, xref: np.ndarray) -> float:
+    """
+    Calculate the root mean square error.
+
+    Parameters
+    ----------
+    x
+        The first array.
+    xref
+        The second array.
+
+    Returns
+    -------
+    rmse
+        The root mean square error.
+    """
+    return np.sqrt(np.mean((x - xref) ** 2))
+
+
+def cum_abs_diff(x: np.ndarray, xref: np.ndarray) -> np.ndarray:
+    """
+    Calculate the cumulative absolute difference between the two arrays.
+
+    Parameters
+    ----------
+    x
+        The first array.
+    xref
+        The second array.
+
+    Returns
+    -------
+    cum_abs_diff
+        The cumulative absolute difference.
+    """
+    return np.cumsum(np.abs(x - xref))
+
+
+def abs_diff(x: np.ndarray, xref: np.ndarray) -> np.ndarray:
+    """
+    Calculate the absolute difference between the two arrays.
+
+    Parameters
+    ----------
+    x
+        The first array.
+    xref
+        The second array.
+
+    Returns
+    -------
+    abs_diff
+        The absolute difference.
+    """
+    return np.abs(x - xref)
