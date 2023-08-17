f527b18dbb0fa60ba794844a7872f4fdff48af38
João Morado <joaomorado@gmail.com>
2023-08-17 15:59:32 +0100

    Add dts as arrays

--- src/track_estimators/kalman_filters/kalman_filter.py
+++ src/track_estimators/kalman_filters/kalman_filter.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple, Union
+
 import numpy as np
 
 from ..ship_track import ShipTrack
@@ -18,11 +20,21 @@ class KalmanFilterBase:
     def __init__(self, *args, **kwargs):
         """Initialise the Kalman filter base class."""
         self.time = 0
+
+        # Control input vector
+        self.c = None
+
+        # Kalman filter predictions
         self.predictions = []
 
     def run(
-        self, nsteps: int, dt: int | float, ship_track: ShipTrack, *args, **kwargs
-    ) -> list:
+        self,
+        nsteps: int,
+        dt: int | float | List[Union[int, float]] | np.ndarray,
+        ship_track: ShipTrack,
+        *args,
+        **kwargs
+    ) -> Tuple[np.ndarray, np.ndarray]:
         """
         Run the algorithm to predict the ship's position based on the given ship_track.
 
@@ -40,23 +52,33 @@ class KalmanFilterBase:
         predictions
             A list of predictions.
         """
+        if isinstance(dt, (list, np.ndarray)):
+            assert len(dt) == nsteps, "dt must be the same length as nsteps"
+        else:
+            dt = np.ones(nsteps) * dt
+
         # Calculate the cumulative time
         time_cumsum = np.cumsum(ship_track.dts)
 
-        major_index = 0
+        update_index = 0
 
         # Save the initial state
         self.predictions.append(self.x)
-        self.update(ship_track.z[:, major_index])
+        # self.update(ship_track.z[:, update_index])
 
         estimate_variance = []
 
-        for step in range(nsteps):
+        self.c = np.asarray(
+            [ship_track.sog[update_index], ship_track.cog[update_index]]
+        )
+
+        for step, dt in enumerate(dt):
             # Predict the next state
             self.predict(
                 dt=dt,
-                sog_rate=ship_track.sog_rate[major_index],
-                cog_rate=ship_track.cog_rate[major_index],
+                c=None,  # self.c,
+                sog_rate=ship_track.sog_rate[update_index],
+                cog_rate=ship_track.cog_rate[update_index],
             )
 
             # Increment time
@@ -64,15 +86,22 @@ class KalmanFilterBase:
 
             # Update step
             if self.time in time_cumsum:
-                major_index += 1
+                update_index += 1
 
-                self.update(ship_track.z[:, major_index])
+                self.c = np.asarray(
+                    [ship_track.sog[update_index], ship_track.cog[update_index]]
+                )
+
+                self.update(ship_track.z[:, update_index])
 
             # Save the predictions
             self.predictions.append(self.x)
             estimate_variance.append(np.diag(self.P))
 
-        return self.predictions, estimate_variance
+        return (
+            np.asarray(self.predictions).squeeze(),
+            np.asarray(estimate_variance).squeeze(),
+        )
 
     def predict(self, *args, **kwargs):
         """Predict the state."""
